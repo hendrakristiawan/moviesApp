0f2a02f7841b4a8dae85741dd6b2c6776d44c802
hendrakristiawan <hendra@jojonomic.com>
2020-09-30 20:41:05 +0700

    implement shared pref

--- app/src/main/java/com/hendra/movieapp/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/hendra/movieapp/ui/login/LoginViewModel.kt
@@ -1,7 +1,10 @@
 package com.hendra.movieapp.ui.login
 
+import android.os.Handler
+import android.os.Looper
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.hendra.movieapp.cache.SharedPref
 import com.hendra.movieapp.utils.ErrorResource
 import com.hendra.movieapp.utils.Resource
 
@@ -10,30 +13,36 @@ class LoginViewModel : ViewModel() {
     private val mutableSignIn: MutableLiveData<Resource<Unit>> = MutableLiveData()
     val signInLiveData: MutableLiveData<Resource<Unit>> = mutableSignIn
 
+    init {
+        checkSession()
+    }
+
+    private fun checkSession() {
+        if (SharedPref.getSessionSignIn()) {
+            signInLiveData.postValue(Resource.success(Unit))
+        }
+    }
+
     fun signIn(email: String, password: String) {
         signInLiveData.postValue(Resource.loading())
+
         when {
             !isValidEmail(email) -> signInLiveData.postValue(Resource.error(ValidationType.INVALID_EMAIL))
             !isValidPassword(password) -> signInLiveData.postValue(Resource.error(ValidationType.INVALID_PASSWORD))
-            else -> signInLiveData.postValue(Resource.success(Unit))
+            else -> {
+                Handler(Looper.getMainLooper()).postDelayed({
+                    SharedPref.saveSuccessSignIn()
+                    signInLiveData.postValue(Resource.success(Unit))
+                }, 3000)
+            }
         }
     }
 
-    private fun isValidEmail(email: String): Boolean {
-        return when {
-            email.isEmpty() -> false
-            !android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches() -> false
-            else -> true
-        }
-    }
+    private fun isValidEmail(email: String): Boolean =
+        email.isNotEmpty() && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
 
-    private fun isValidPassword(password: String): Boolean {
-        return when {
-            password.isEmpty() -> false
-            password.length < 8 -> false
-            else -> true
-        }
-    }
+    private fun isValidPassword(password: String): Boolean =
+        password.isNotEmpty() && password.length >= 8
 }
 
 enum class ValidationType : ErrorResource {
